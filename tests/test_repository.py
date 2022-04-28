84e221a9634d431628f7e10657f508a49a02d75a
Aleksandr <dev.aleksan@gmail.com>
2022-04-28 20:29:15 +0300

    feat: add test repository.

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,5 +1,5 @@
 from app import repository
-from app.model import Batch
+from app.model import Batch, OrderLine
 
 
 def test_repository_can_save_a_batch(session):
@@ -16,3 +16,56 @@ def test_repository_can_save_a_batch(session):
         )
     )
     assert rows == [('batch1', 'ИЗЫСКАННАЯ-СКАМЕЙКА', 100, None)]
+
+
+def insert_order_line(session) -> dict:
+    """Добавить строку заказа."""
+    session.execute(
+        "INSERT INTO order_lines (orderid, sku, qty)"
+        ' VALUES ("order1", "БОЛЬШОЙ-ДИВАН", 12)'
+    )
+    [[orderline_id]] = session.execute(
+        "SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku",
+        dict(orderid="order1", sku="БОЛЬШОЙ-ДИВАН"),
+    )
+    return orderline_id
+
+
+def insert_batch(session, batch_id) -> dict:
+    session.execute(
+        "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
+        ' VALUES (:batch_id, "БОЛЬШОЙ-ДИВАН", 100, null)',
+        dict(batch_id=batch_id),
+    )
+    [[batch_id]] = session.execute(
+        'SELECT id FROM batches WHERE reference=:batch_id AND sku="БОЛЬШОЙ-ДИВАН"',
+        dict(batch_id=batch_id),
+    )
+
+    return batch_id
+
+
+def insert_allocation(session, orderline_id, batch_id) -> None:
+    session.execute(
+        "INSERT INTO allocations (orderline_id, batch_id)"
+        " VALUES (:orderline_id, :batch_id)",
+        dict(orderline_id=orderline_id, batch_id=batch_id),
+    )
+
+
+def test_repository_can_retrieve_a_batch_with_allocations(session):
+    orderline_id = insert_order_line(session)
+    batch1_id = insert_batch(session, "batch1")
+    insert_batch(session, "batch2")
+    insert_allocation(session, orderline_id, batch1_id)
+
+    repo = repository.SqlAlchemyRepository(session)
+    retrieved = repo.get("batch1")  # type:ignore
+
+    expected = Batch("batch1", "БОЛЬШОЙ-ДИВАН", 100, eta=None)
+    assert retrieved == expected  # Batch.__eq__ only compares reference
+    assert retrieved.sku == expected.sku
+    assert retrieved._purchased_quantity == expected._purchased_quantity
+    assert retrieved._allocations == {
+        OrderLine("order1", "БОЛЬШОЙ-ДИВАН", 12),
+    }
